ae852a3adbf0f142aa88c3b667cb156fd1df4f59
aizjicod <98921366+aizjicod@users.noreply.github.com>
2022-06-21 19:44:52 -0400

    fix linter error

--- js/artist.js
+++ js/artist.js
@@ -6,7 +6,7 @@ const infoArtist = [
     patternAlt: 'img/artist/swirl_pattern.webp',
     name: 'BoyWithUke',
     genre: 'indie, hip-hop/rap',
-    music: '"understand", "toxic", "two moons" and many more.'
+    music: '"understand", "toxic", "two moons" and many more.',
   },
   {
     img: 'img/artist/blackbear-removebg-preview.png',
@@ -15,7 +15,7 @@ const infoArtist = [
     patternAlt: 'img/artist/swirl_pattern.webp',
     name: 'Blackbear',
     genre: 'urban music, pop',
-    music: '“idfc”, “me & ur ghost” and “hot girl bummer”.'
+    music: '“idfc”, “me & ur ghost” and “hot girl bummer”.',
   },
   {
     img: 'img/artist/doja_cat-removebg-preview.png',
@@ -24,7 +24,7 @@ const infoArtist = [
     patternAlt: 'img/artist/swirl_pattern.webp',
     name: 'Doja Cat',
     genre: 'Pop R&B Hip hop Pop rap',
-    music: '"Say So", "Juicy", "Like That", "Kiss Me More".'
+    music: '"Say So", "Juicy", "Like That", "Kiss Me More".',
   },
   {
     img: 'img/artist/paulolondra-removebg-preview.png',
@@ -33,7 +33,7 @@ const infoArtist = [
     patternAlt: 'img/artist/swirl_pattern.webp',
     name: 'Paulo Londra',
     genre: '"trap", "urban", "freestyle", "rap"',
-    music: '"plan A", "yo no se", "no puedo" and many more.'
+    music: '"plan A", "yo no se", "no puedo" and many more.',
   },
   {
     img: 'img/artist/postmalone-removebg-preview.png',
@@ -42,7 +42,7 @@ const infoArtist = [
     patternAlt: 'img/artist/swirl_pattern.webp',
     name: 'Post malone',
     genre: 'Hip-hop/rap',
-    music: '“sunflower”, “congratulations” and “circles.”'
+    music: '“sunflower”, “congratulations” and “circles.”',
   },
   {
     img: 'img/artist/subruban-removebg-preview.png',
@@ -51,40 +51,41 @@ const infoArtist = [
     patternAlt: 'img/artist/swirl_pattern.webp',
     name: 'Sub urban',
     genre: 'pop, Pop indie, Electrónica',
-    music: '"cradles", "inferno", "freak" and many more.'
-  }
-]
+    music: '"cradles", "inferno", "freak" and many more.',
+  },
+];
 
-const cardWrapper = document.querySelector('.card-wrapper')
+const cardWrapper = document.querySelector('.card-wrapper');
 const createCard = (artist) => {
   // creating the card of the artist
   const artistCard = document.createElement('div');
-  artistCard.classList.add('card-artist')
+  artistCard.classList.add('card-artist');
 
   // portrait
   const portrait = document.createElement('img');
   portrait.setAttribute('src', artist.img);
   portrait.setAttribute('alt', artist.imgAlt);
-  artistCard.appendChild(portrait)
+  artistCard.appendChild(portrait);
 
   // pattern of the image
   const pattern = document.createElement('img');
   pattern.setAttribute('src', artist.pattern);
   pattern.setAttribute('alt', artist.patternAlt);
-  artistCard.appendChild(pattern)
+  artistCard.appendChild(pattern);
 
   // create content container
   const divContainer = document.createElement('div');
-  divContainer.classList.add('card-content')
-
-  // name of artist 
+  divContainer.classList.add('card-content');
+  
+  // name of artist
   const name = document.createElement('h2');
+  name.classList.add('content-h2');
   name.textContent = artist.name;
   divContainer.appendChild(name);
 
   // genre of artist
   const genre = document.createElement('h3');
-  genre.classList.add('genre')
+  genre.classList.add('genre');
   genre.textContent = `genre : ${artist.genre}`;
   divContainer.appendChild(genre);
 
@@ -93,10 +94,10 @@ const createCard = (artist) => {
   music.textContent = `${artist.name} will be platying: ${artist.music}`;
   divContainer.appendChild(music);
 
-  artistCard.appendChild(divContainer)
-  cardWrapper.appendChild(artistCard)
-}
+  artistCard.appendChild(divContainer);
+  cardWrapper.appendChild(artistCard);
+};
 
-infoArtist.forEach((artist)=> {
-  createCard(artist)
+infoArtist.forEach((artist) => {
+  createCard(artist);
 });
--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
-const menu = document.querySelector(".burger-btn");
-const header = document.querySelector('header')
+const menu = document.querySelector('.burger-btn');
+const header = document.querySelector('header');
 
 menu.addEventListener('click', () => {
-  menu.classList.toggle('active')
-  header.classList.toggle('active')
-})
+  menu.classList.toggle('active');
+  header.classList.toggle('active');
+});
